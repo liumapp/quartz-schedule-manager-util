dfdd643a6d41788796337d6573db0ab8ebd08df1
liumapp <liumapp.com@gmail.com>
2018-01-08 15:31:11 +0800

    add time and unit

--- src/main/java/com/liumapp/schedule/util/container/JobInfo.java
+++ src/main/java/com/liumapp/schedule/util/container/JobInfo.java
@@ -1,5 +1,7 @@
 package com.liumapp.schedule.util.container;
 
+import org.quartz.DateBuilder;
+
 /**
  * Created by liumapp on 1/8/18.
  * E-mail:liumapp.com@gmail.com
@@ -12,6 +14,10 @@ public class JobInfo {
     private String triggerName ;
 
     private String groupName ;
+
+    private Integer time;
+
+    private DateBuilder.IntervalUnit unit;
 
     public String getJobName() {
         return jobName;
@@ -35,5 +41,21 @@ public class JobInfo {
 
     public void setGroupName(String groupName) {
         this.groupName = groupName;
+    }
+
+    public Integer getTime() {
+        return time;
+    }
+
+    public void setTime(Integer time) {
+        this.time = time;
+    }
+
+    public DateBuilder.IntervalUnit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(DateBuilder.IntervalUnit unit) {
+        this.unit = unit;
     }
 }
