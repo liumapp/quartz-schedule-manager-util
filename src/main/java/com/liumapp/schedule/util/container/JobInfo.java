b593f3637e97ef3bc85477b9081703ed225de536
liumapp <liumapp.com@gmail.com>
2018-01-08 11:15:46 +0800

    use jobcontainer instead of pattern

---
+++ src/main/java/com/liumapp/schedule/util/container/JobInfo.java
@@ -0,0 +1,12 @@
+package com.liumapp.schedule.util.container;
+
+/**
+ * Created by liumapp on 1/8/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class JobInfo {
+
+
+
+}
